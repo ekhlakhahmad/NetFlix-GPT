2c560c80613933cdd3e076f851328efd4be73419
Ekhlakh Ahmad <ekhlakhahmad95@gmail.com>
2024-07-09 23:55:24 +0530

    xyz

--- src/components/Firebase/Firebase.jsx
+++ src/components/Firebase/Firebase.jsx
@@ -1,5 +1,9 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
 	apiKey: "AIzaSyDpvm1xHjyoWJXWQKbn1huu7UP7F3dMfq8",
 	authDomain: "netflix-gpt-8c3a6.firebaseapp.com",
@@ -9,4 +13,5 @@ const firebaseConfig = {
 	appId: "1:1009392673838:web:a3a6e30efa76a53a4c6bbf",
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
