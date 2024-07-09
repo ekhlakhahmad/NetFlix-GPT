880b499e9c0830194f5ac7bff76613fce226da8f
Ekhlakh Ahmad <ekhlakhahmad95@gmail.com>
2024-07-09 23:53:44 +0530

    abc

---
+++ src/components/Firebase/Firebase.jsx
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyDpvm1xHjyoWJXWQKbn1huu7UP7F3dMfq8",
+	authDomain: "netflix-gpt-8c3a6.firebaseapp.com",
+	projectId: "netflix-gpt-8c3a6",
+	storageBucket: "netflix-gpt-8c3a6.appspot.com",
+	messagingSenderId: "1009392673838",
+	appId: "1:1009392673838:web:a3a6e30efa76a53a4c6bbf",
+};
+
+const app = initializeApp(firebaseConfig);
